cb86b3a0547bba8eabfc918688b79d956e358d94
Oshanlakshith <oshanlakshitha146@gmail.com>
2022-06-23 12:12:30 +0530

    Repositorys add

---
+++ SpringPos/CourceWork/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.propertiies")
+public class JPAConfig {
+
+}
